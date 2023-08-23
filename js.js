f25ea3b24720047fa7449c540ca5a5acce8494a1
Shamimreza82 <shamimrezaone@gmail.com>
2023-08-23 13:45:22 +0600

    dfault paramiter

---
+++ js.js
@@ -0,0 +1,11 @@
+/// dfalt paramiter
+
+// function add (num1 = 0, num2 = 0){
+//     const result = num1 + num2; 
+//     console.log (num1, num2, result)
+//     return result; 
+// }
+
+// const sum = add(3);
+
+
